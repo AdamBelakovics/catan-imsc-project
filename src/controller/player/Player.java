a99559c758e17ffc4609ccc00a9c86890d5b2d1d
Adam <Adam@152.66.177.94>
2016-11-17 20:39:56 +0100

    test git

--- src/controller/player/Player.java
+++ src/controller/player/Player.java
@@ -11,7 +11,7 @@ public class Player {
 	int id;
 	int points;
 	Map<Resource, Integer> changeLUT;
-	
+	//mate test gitt
 	
 	/**
 	 * Rolls the dice(e. g. generates two random integers (1-6), and adds them), and allocates new resources to each Player.
