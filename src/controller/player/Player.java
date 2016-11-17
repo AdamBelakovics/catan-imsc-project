af79af01f1545e2d55e9ac7adddd1b5eb590fcfd
Hollo1996 <holloszaboakos.1996@gmail.com>
2016-11-17 21:30:09 +0100

    third step

--- src/controller/player/Player.java
+++ src/controller/player/Player.java
@@ -11,7 +11,7 @@ public class Player {
 	int id;
 	int points;
 	Map<Resource, Integer> changeLUT;
-	//Akos is here! Lets work!
+	//Akos is here! Lets work!5
 	
 	/**
 	 * Rolls the dice(e. g. generates two random integers (1-6), and adds them), and allocates new resources to each Player.
