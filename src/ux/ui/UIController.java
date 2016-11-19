5f8cbde46c5b5b2bd29f1ef79503eb970d35dd2a
kisss <kisss@Muricidae>
2016-11-19 15:22:32 +0100

    Changed PlayerController class to public

---
+++ src/ux/ui/UIController.java
@@ -0,0 +1,21 @@
+package ux.ui;
+
+import java.util.Map;
+
+import controller.player.*;
+
+public class UIController extends PlayerController {
+
+	@Override
+	public boolean query(Player donor, Map<Resource, Integer> offer, Map<Resource, Integer> demand) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void turn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
