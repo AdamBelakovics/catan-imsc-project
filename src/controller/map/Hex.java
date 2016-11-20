57d5f6b8641cf8c6852c32ac46e92097c57c1e16
AdamBelakovics <beladi96@gmail.com>
2016-11-20 02:24:42 +0100

    Update Hex.java

--- src/controller/map/Hex.java
+++ src/controller/map/Hex.java
@@ -30,12 +30,6 @@ public class Hex implements TableElement {
 		return ypos;
 	}
 	
-	@Override
-	public boolean isBuildPossible(Building what) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
 	private void generateVertices() {
 		for(Orientation o : Orientation.values()){
 			if(o == Orientation.EAST || o == Orientation.WEST){
