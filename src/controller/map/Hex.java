b866bbb20c42ef960166d29771f3162596fe990d
AdamBelakovics <beladi96@gmail.com>
2016-11-20 02:28:06 +0100

    Update Hex.java

--- src/controller/map/Hex.java
+++ src/controller/map/Hex.java
@@ -2,33 +2,27 @@
 
 package controller.map;
 
-import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.HashMap;
-import java.util.Map;
+
 
 import controller.player.Building;
 
 
 public class Hex implements TableElement {
 	HashMap<Orientation, Vertex> vertices = new HashMap<Orientation, Vertex>(6);
-	int xpos;
-	int ypos;
 	String id;
 	
-	public Hex (int x, int y){
-		id = Integer.toString(x) + Integer.toString(y);
-		xpos = x;
-		ypos = y;
+	public Hex (String identifier){
+		id = identifier;
 	}
 	
-	public int getx(){
-		return xpos;
+	public String getID(){
+		return id;
 	}
 	
-	public int gety(){
-		return ypos;
-	}
+	
+	
 	
 	private void generateVertices() {
 		for(Orientation o : Orientation.values()){
@@ -41,20 +35,6 @@ public class Hex implements TableElement {
 		}
 	}
 	
-	
-//for testing
-	static public Polygon drawableHex(double x, double y, double r){
-	    double pi = Math.PI;
-	    int[] xPoints = new int[6];
-	    int[] yPoints = new int[6];
-	    int nPoints = 6;
-		for(int i=0; i<6; i++) {
-		    xPoints[i] = (int) (x + r*Math.cos(i*2*pi/6 + pi/6));
-		    yPoints[i] = (int) (y + r*Math.sin(i*2*pi/6 + pi/6));
-		}
-		return new Polygon(xPoints, yPoints, nPoints);
-	}
-//for testing end
 
 	@Override
 	public boolean isBuildPossible(Building what) {
