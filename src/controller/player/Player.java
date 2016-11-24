c9257a8be6dc407a178f96a5ad0aec38784c9d8f
AdamBelakovics <beladi96@gmail.com>
2016-11-24 19:19:40 +0100

    Clarified class Player, added buyDevCard method, added comments

--- src/controller/player/Player.java
+++ src/controller/player/Player.java
@@ -20,15 +20,6 @@ public class Player {
 	ArrayList<DevCard> playedDevCards = new ArrayList<DevCard>();
 	
 	/**
-	 * TODO work in progress
-	 * @param playerID
-	 * @return
-	 */
-	public ArrayList<DevCard> getPlayedDevelopmentCards(){
-		return playedDevCards;	
-	}
-	
-	/**
 	 * Player constructor
 	 * @param name
 	 * @param id
@@ -36,7 +27,6 @@ public class Player {
 	 * Initialize changeLUT to 4
 	 * Initialize resourcePool to 0
 	 */
-	
 	public Player(String name, int id, PlayerController controller){
 		this.name = name;
 		this.id = id;
@@ -54,6 +44,8 @@ public class Player {
 				
 	}
 	
+	//PLAYER GETTER/SETTER METHODS PRIVATE---------------------------------------------------------------------->
+	//implemented as protected final for JUNIT testing
 	
 	/**
 	 * Increase player's points with a value
@@ -61,8 +53,7 @@ public class Player {
 	 * @throws GameEndsException if points are bigger then 10
 	 * TODO Handle exception in main!
 	 *  **/
-	
-	public void incPoints(int value) throws GameEndsException{			
+	protected final void incPoints(int value) throws GameEndsException{			
 		if((points + value) >= 10){	
 			points = 10;
 			throw new GameEndsException(id);						 		
@@ -74,33 +65,26 @@ public class Player {
 	 * @param value
 	 * @throws OutOfRangeException if (points - value) goes negative
 	 */
-	public void decPoints(int value) throws OutOfRangeException{
+	protected final void decPoints(int value) throws OutOfRangeException{
 		if((points - value) < 0) throw new OutOfRangeException("Points goes negative");
 		points -= value;
 	}
 	
 	/**
-	 * getChangeLUT
-	 * @param Resource
-	 * @return value of change lut
+	 * Number of active knights can be increased by a given amount with this method.
+	 * @param value 
 	 */
-	public int getChangeLUT(Resource r){
-		return changeLUT.get(r);
+	protected final void incActiveKnights(int value){
+		activeKnights += value;
 	}
+	
 	/**
-	 * set ChangeLUT
-	 * @param Resource r
-	 * @param int  value
-	 * @throws OutOfRangeException if value not 2, 3 or 4
+	 * Number of active knights can be decreased by a given amount with this method.
+	 * @param value
 	 */
-	public void setChangeLUT(Resource r, int value)throws OutOfRangeException{
-		if(value!=2 || value!=3 || value!=4) throw new OutOfRangeException("Invalid value. (Try 2, 3, 4)");
-		changeLUT.replace(r, value);
-	}
-	
-	
-	public int getResourcePool(Resource r){
-		return resourcePool.get(r);
+	protected final void decActiveKnights(int value) throws OutOfRangeException{
+		if((activeKnights - value) < 0) throw new OutOfRangeException("It can't be negative.");
+		activeKnights -= value;
 	}
 	
 	/**
@@ -109,8 +93,7 @@ public class Player {
 	 * @param int value 
 	 * @throws OutOfRangeException if value is negative
 	 */
-	
-	public void incResourcePool(Resource r, int value) throws OutOfRangeException{
+	protected final void incResourceAmount(Resource r, int value) throws OutOfRangeException{
 		if(value < 0) throw new OutOfRangeException("Value can't be negative.");
 			resourcePool.replace(r, (resourcePool.get(r)+value));		
 	}
@@ -121,55 +104,106 @@ public class Player {
 	 * @param int value 
 	 * @throws OutOfRangeException if value is negative OR with this value, player's stuff goes below 0
 	 */
-	public void decResourcePool(Resource r, int value) throws OutOfRangeException{
+	protected final void decResourceAmount(Resource r, int value) throws OutOfRangeException{
 		if(value < 0) throw new OutOfRangeException("Value can't be negative.");
 		if((resourcePool.get(r)-value) < 0) throw new OutOfRangeException("It can not be much reduced");
 		resourcePool.replace(r, (resourcePool.get(r)-value));		
 	}
 	
+	/**
+	 * set ChangeLUT
+	 * @param Resource r
+	 * @param int  value
+	 * @throws OutOfRangeException if value not 2, 3 or 4
+	 */
+	protected final void setChangeLUT(Resource r, int value)throws OutOfRangeException{
+		if(value!=2 || value!=3 || value!=4) throw new OutOfRangeException("Invalid value. (Try 2, 3, 4)");
+		changeLUT.replace(r, value);
+	}
+	
+	//PLAYER GETTER/SETTER METHODS PUBLIC---------------------------------------------------------------------->
+	
+	/**
+	 * getChangeLUT
+	 * @param Resource
+	 * @return value of change lut
+	 */
+	public int getChangeLUT(Resource r){
+		return changeLUT.get(r);
+	}
+	
+	/**
+	 * Getter for Resources the Player currently has.
+	 * @param Resource r is the Resource you want to get the amount of
+	 * @return int the amount of a specified Resource
+	 */
+	public int getResourceAmount(Resource r){
+		return resourcePool.get(r);
+	}
+	
+	/**
+	 * Getter for played DevCards
+	 * @param playerID
+	 * @return
+	 */
+	public ArrayList<DevCard> getPlayedDevelopmentCards(){
+		return playedDevCards;	
+	}
+	
+	/**
+	 * Getter for PlayerController.
+	 * @return PlayerController associated with this player.
+	 */
 	public PlayerController getPlayerController(){
 		return controller;
 	}
 	
+	/**
+	 * Getter for Player's name.
+	 * @return Player's name
+	 */
 	public String getName(){
 		return name;
 	}
 	
-	
+	/**
+	 * Getter for Player's ID
+	 * @return Player's ID
+	 */
 	public int getId(){
 		return id;
 	}
 	
+	/**
+	 * Getter for Player's points
+	 * @return Player's points
+	 */
 	public int getPoints(){
 		return points;
 	}
 	
+	/**
+	 * Getter for the number of active knights.
+	 * @return number of active knights
+	 */
 	public int getActiveKnights(){
 		return activeKnights;
 	}
 	
-	public void incActiveKnights(int value){
-		activeKnights += value;
-	}
 	
-	public void decActiveKnights(int value) throws OutOfRangeException{
-		if((activeKnights - value) < 0) throw new OutOfRangeException("It can not be much reduced.");
-		activeKnights -= value;
-	}
-	
-	
-	
-	
+	//PLAYER ACTIONS---------------------------------------------------------------------->
 	
 	/**
+	 * PLAYER ACTION
 	 * Rolls the dice(e. g. generates two random integers (1-6), and adds them), and allocates new resources to each Player.
 	 * If 7 is rolled calls the handleThief method
 	 */
 	public void rollTheDice() {
-		handleThief(); //ennek itt kell lennie
+		handleThief(); //ennek itt kell majd lennie TODO
 	}
 	
 	/**
+	 * PLAYER ACTION
 	 * The player can choose to change the position of the Thief. Executes all the changes accordingly.
 	 * 
 	 */
@@ -178,6 +212,7 @@ public class Player {
 	}
 	
 	/**
+	 * PLAYER ACTION
 	 * Builds a Building to a specific TableElement
 	 * @param what Reference for a Building
 	 * @param where Reference to the TableElement
@@ -187,6 +222,7 @@ public class Player {
 	}
 	
 	/**
+	 * PLAYER ACTION
 	 * Implements trading TODO
 	 * Uses the PlayerController.query() function to make offers.
 	 * Modifies resources accordingly.
@@ -196,6 +232,16 @@ public class Player {
 	}
 	
 	/**
+	 * PLAYER ACTION
+	 * Player can buy a random Development Card from the DevCardShop.
+	 * It's added to the devCards List.
+	 */
+	public void buyDevCard(){
+		
+	}
+	
+	/**
+	 * PLAYER ACTION
 	 * Uses one of the players Dev cards
 	 * @param dc
 	 */
