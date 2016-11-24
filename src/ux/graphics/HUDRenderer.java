f9f0bcad8c2414e6a226acb656b37ddbbb3112b4
kisss <kisss@Muricidae>
2016-11-24 12:14:30 +0100

    Added basic interface elements

--- src/ux/graphics/HUDRenderer.java
+++ src/ux/graphics/HUDRenderer.java
@@ -1,7 +1,25 @@
 package ux.graphics;
 
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
-public class HUDRenderer extends JPanel {
+public class HUDRenderer extends ImageRenderer {
+	Graphics2D hudCanvas;
+	BufferedImage interfaceImage;
+	InterfaceRenderer interfaceRenderer;
+	
 
+	public HUDRenderer(int _width, int _height) {
+		width=_width;
+		height=_height;
+		interfaceRenderer=new InterfaceRenderer(width,height);
+		interfaceImage=new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	}
+
+	public void paint(Graphics g) {
+		hudCanvas=(Graphics2D)g;
+		interfaceRenderer.paint(interfaceImage.getGraphics());
+		if (interfaceImage!=null) hudCanvas.drawImage(interfaceImage, 0, 0, null);
+	}
 }
