ebfa20b77bed740788c384fd13a45b24f7955bd0
AdamBelakovics <beladi96@gmail.com>
2016-11-25 13:54:42 +0100

    Started implementing main thread

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,22 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import controller.map.Table;
+import controller.player.Player;
+import ux.ui.UIController;
+
 public class Controller {
 	public static void main(String[] args){
+		//MENÜ létrehoz
+		//MENÜnek átadjuk az irányítást
+		//MENÜ visszatér TODO kezelni
+		Table gameTable = new Table();
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(new Player("Alfonz", 0, null));
 		
 	}
 }
+
