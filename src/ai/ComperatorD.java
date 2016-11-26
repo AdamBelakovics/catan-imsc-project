76221e929c20a156b1d766e39c4347c99c6fa101
Hollo1996 <holloszaboakos.1996@gmail.com>
2016-11-26 17:43:36 +0100

    Add files via upload
    A jó komperátor.. A másikat nem tom hogy lehet törölni xdd

---
+++ src/ai/ComperatorD.java
@@ -0,0 +1,12 @@
+package ai;
+
+import java.util.Comparator;
+
+public class ComperatorD implements Comparator<Double> {
+	@Override
+	public int compare(Double o1, Double o2) {
+		// TODO Auto-generated method stub
+		return (o1).compareTo(o2);
+	}
+
+}
