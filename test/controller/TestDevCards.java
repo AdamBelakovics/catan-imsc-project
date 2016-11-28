ed5747a6de57ab9c65bc72eaa02308c7c6c15db4
MateSzoke <szokemate@yahoo.com>
2016-11-28 18:46:47 +0100

    Not finished yet!

---
+++ test/controller/TestDevCards.java
@@ -0,0 +1,62 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import controller.player.NotEnoughResourcesException;
+import controller.player.OutOfRangeException;
+import controller.player.Player;
+import controller.player.Resource;
+import controller.player.devcards.DevCard;
+import controller.player.devcards.DevCardShop;
+
+
+
+public class TestDevCards {
+
+	Player player = new Player("Adolf", 1, null);
+	Resource w = Resource.Wool;
+	Resource o = Resource.Ore;
+	Resource g = Resource.Grain;
+	
+	
+	@Before
+	public void setUp() throws OutOfRangeException, NotEnoughResourcesException{
+	
+
+	
+	player.incResourceAmount(w, 100);
+	player.incResourceAmount(g, 100);
+	player.incResourceAmount(o, 100);
+	player.buyDevCard();
+	player.buyDevCard();
+	player.buyDevCard();
+	player.buyDevCard();
+	player.buyDevCard();
+	
+
+	}
+	
+	@Test
+	public void testWhoolAmount() {
+		assertEquals(95, player.getResourceAmount(w) );
+	}
+	
+	@Test
+	public void testGrainAmount() {
+		assertEquals(95, player.getResourceAmount(g) );
+	}
+	
+	@Test
+	public void testDevCardAmount(){
+		assertEquals(5, player.getOwnDevCards().size());
+	}
+	
+	
+
+}
