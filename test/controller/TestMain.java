6a2b3c044515f59a3cdc7f760c7f83ab542627bb
AdamBelakovics <beladi96@gmail.com>
2016-11-28 20:49:02 +0100

    test written

--- test/controller/TestMain.java
+++ test/controller/TestMain.java
@@ -8,6 +8,8 @@ import controller.player.GameEndsException;
 import controller.player.OutOfRangeException;
 import controller.player.Player;
 import controller.player.Resource;
+import ux.Renderer;
+import ux.ui.UIController;
 
 public class TestMain {
 
@@ -17,6 +19,10 @@ public class TestMain {
 		ArrayList<Player> players = new ArrayList<Player>();
 		players.add(p);
 		Game theGame = new Game(t, players);
+		UIController u = new UIController();
+		u.setControlledPlayer(p);
+		p.setPlayerController(u);
+		Renderer r = new Renderer(u, t, 1280, 720);
 		p.incResourceAmount(Resource.Lumber, 100);
 		p.incResourceAmount(Resource.Brick, 100);
 		p.incResourceAmount(Resource.Wool, 100);
@@ -26,7 +32,6 @@ public class TestMain {
 			p.firstBuild(Buildable.Settlement, t.getNodes().get(4));
 			p.firstBuild(Buildable.Road, t.getNodes().get(3).getNeighbourEdges().get(0));
 			System.out.println(p.build(Buildable.Road, t.getNodes().get(3).getNeighbourEdges().get(1)));
-
 		} catch (GameEndsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
