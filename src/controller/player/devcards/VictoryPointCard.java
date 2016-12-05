1af1978c2d6a36e685e9e79ed57aee3302be00ac
szoke <szoke@152.66.177.148>
2016-12-05 21:32:16 +0100

    előbb eerontottam, mos' jó

--- src/controller/player/devcards/VictoryPointCard.java
+++ src/controller/player/devcards/VictoryPointCard.java
@@ -7,13 +7,8 @@ import controller.player.Resource;
 public class VictoryPointCard extends DevCard{
 	
 	@Override
-	public void doCard(Player p) {
-		try {
-			p.incPoints(1);
-		} catch (GameEndsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void doCard(Player p) throws GameEndsException {
+		p.incPoints(1);
 	}
 	
 }
