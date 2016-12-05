6d65311912da66d3e293fdedbacfdfa5f5ff0e82
szoke <szoke@152.66.177.148>
2016-12-05 23:21:13 +0100

    Devcardbuy bug fixed

--- src/controller/player/Player.java
+++ src/controller/player/Player.java
@@ -751,8 +751,8 @@ public class Player {
 		Resource w = Resource.Wool;
 		Resource o = Resource.Ore;
 		Resource g = Resource.Grain;
-		if(!DevCardShop.isShopEmpty()){
-			if((resourcePool.get(w)<0) | (resourcePool.get(o)<0) |  (resourcePool.get(g)<0)) throw new NotEnoughResourcesException("You dont't have enough resoureces to buy developement card.");
+		if(!DevCardShop.isShopEmpty() && !(resourcePool.get(w)<1) | !(resourcePool.get(o)<1) |  !(resourcePool.get(g)<1)){
+			
 			try {
 				decResourceAmount(w, 1);
 				decResourceAmount(o, 1);
