16f79b41e15286ff4748b0114d4bbdd70e3019a7
szoke <szoke@152.66.177.148>
2016-12-05 21:30:50 +0100

    bla

--- src/controller/player/devcards/VictoryPointCard.java
+++ src/controller/player/devcards/VictoryPointCard.java
@@ -7,7 +7,7 @@ import controller.player.Resource;
 public class VictoryPointCard extends DevCard{
 	
 	@Override
-	public void doCard(Player p, Resource r) {
+	public void doCard(Player p) {
 		try {
 			p.incPoints(1);
 		} catch (GameEndsException e) {
