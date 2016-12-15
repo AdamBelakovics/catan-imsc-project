68e77f32a764ef4d8dc032c34eb53ded82e1b9bd
Gergely Olah <olahkoo@gmail.com>
2016-12-15 19:43:53 +0100

    More than 4 ais can play
    If more than 4 ais play, first four of them will be picked
    to play in each game. If random is set to true, 4 random ais
    will play in each game.

--- src/aitest/TestMain.java
+++ src/aitest/TestMain.java
@@ -31,6 +31,7 @@ import controller.player.devcards.DevCard;
 import controller.player.devcards.DevCardShop;
 import controller.player.devcards.KnightCard;
 import controller.player.devcards.VictoryPointCard;
+import javafx.print.PageLayout;
 import ux.Renderer;
 import ux.ui.UIController;
 
@@ -38,28 +39,67 @@ public class TestMain {
 	public static void main(String[] args) throws InterruptedException, OutOfRangeException{
 		ArrayList<HashSet<AiParameter>> paramSets = new ArrayList<>();
 		HashSet<AiParameter> paramSet1 = new HashSet<>();
-		paramSet1.add(AiParameter.NewRes);
-		paramSet1.add(AiParameter.Port);
+		paramSet1.add(AiParameter.Random);
+		//paramSet1.add(AiParameter.Port);
 		//paramSet1.add(AiParameter.Orain);
 		HashSet<AiParameter> paramSet2 = new HashSet<>();
-		paramSet2.add(AiParameter.NewRes);
-		paramSet2.add(AiParameter.Port);
-		//paramSet2.add(AiParameter.Orain);
+		//paramSet2.add(AiParameter.NewRes);
+		//paramSet2.add(AiParameter.Port);
+		paramSet2.add(AiParameter.Random);
 		HashSet<AiParameter> paramSet3 = new HashSet<>();
-		paramSet3.add(AiParameter.NewRes);
-		paramSet3.add(AiParameter.Port);
+		paramSet3.add(AiParameter.Random);
+		//paramSet3.add(AiParameter.Port);
 		//paramSet3.add(AiParameter.Lumbrick);
 		HashSet<AiParameter> paramSet4 = new HashSet<>();
-		paramSet4.add(AiParameter.NewRes);
+		//paramSet4.add(AiParameter.NewRes);
 		paramSet4.add(AiParameter.Port);
 		//paramSet4.add(AiParameter.Lumbrick);
+		HashSet<AiParameter> paramSet5 = new HashSet<>();
+		paramSet5.add(AiParameter.NewRes);
+		paramSet5.add(AiParameter.Port);
+		//paramSet5.add(AiParameter.Orain);
+		HashSet<AiParameter> paramSet6 = new HashSet<>();
+		//paramSet6.add(AiParameter.NewRes);
+		//paramSet6.add(AiParameter.Port);
+		paramSet6.add(AiParameter.Orain);
+		HashSet<AiParameter> paramSet7 = new HashSet<>();
+		paramSet7.add(AiParameter.Lumbrick);
+		HashSet<AiParameter> paramSet8 = new HashSet<>();
+		paramSet8.add(AiParameter.NewRes);
+		paramSet8.add(AiParameter.Orain);
+		HashSet<AiParameter> paramSet9 = new HashSet<>();
+		paramSet9.add(AiParameter.NewRes);
+		paramSet9.add(AiParameter.Lumbrick);
+		HashSet<AiParameter> paramSet10 = new HashSet<>();
+		paramSet10.add(AiParameter.Orain);
+		paramSet10.add(AiParameter.Port);
+		HashSet<AiParameter> paramSet11 = new HashSet<>();
+		paramSet11.add(AiParameter.Lumbrick);
+		paramSet11.add(AiParameter.Port);
+		HashSet<AiParameter> paramSet12 = new HashSet<>();
+		paramSet12.add(AiParameter.NewRes);
+		paramSet12.add(AiParameter.Port);
+		paramSet12.add(AiParameter.Orain);
+		HashSet<AiParameter> paramSet13 = new HashSet<>();
+		paramSet13.add(AiParameter.NewRes);
+		paramSet13.add(AiParameter.Lumbrick);
+		paramSet13.add(AiParameter.Port);
 		paramSets.add(paramSet1);
 		paramSets.add(paramSet2);
 		paramSets.add(paramSet3);
-		paramSets.add(paramSet4);
+		/*paramSets.add(paramSet4);
+		paramSets.add(paramSet5);
+		paramSets.add(paramSet6);
+		paramSets.add(paramSet7);
+		paramSets.add(paramSet8);
+		paramSets.add(paramSet9);
+		paramSets.add(paramSet10);
+		paramSets.add(paramSet11);
+		paramSets.add(paramSet12);
+		paramSets.add(paramSet13);*/
 		// turns, players count, random, is there a stupid player, paramsets
-		run(100, 4, false, false, paramSets);
-		//interactiveGameplay(4, false, false, paramSets);
+		//run(1, 3, true, true, paramSets);
+		interactiveGameplay(3, false, true, paramSets);
 		//printStatus();
 	}
 	private static void interactiveGameplay(int playerCnt, boolean random, boolean isStupid, ArrayList<HashSet<AiParameter>> paramSets){
@@ -120,23 +160,32 @@ public class TestMain {
 	 * @param paramSets - list of the ais' parameter sets, size must be same as playerCnt
 	 */
 	private static void run(int turns, int playerCnt, boolean random, boolean isStupid, ArrayList<HashSet<AiParameter>> paramSets){
+		int competitorCnt = paramSets.size();
+		if(playerCnt > competitorCnt){
+			System.out.println("Legfeljebb annyian jatszhatnak, ahany parameterhalmazt megadtal!");
+			return;
+		}
 		GregorianCalendar startTime = new GregorianCalendar();
 		
 		ArrayList<Integer> aiWins = new ArrayList<>();
 		ArrayList<Integer> aiPoints = new ArrayList<>();
+		ArrayList<Integer> aiGames = new ArrayList<>();
 		ArrayList<Integer> aiGameTurns = new ArrayList<>();
+		ArrayList<Integer> aiWinGameTurns = new ArrayList<>();
 		ArrayList<Integer> aiMinTurns = new ArrayList<>();
 		ArrayList<Integer> aiMaxTurns = new ArrayList<>();
 		
-		for(int i = 0; i < playerCnt; i++){
+		for(int i = 0; i < competitorCnt; i++){
 			aiWins.add(0);
 			aiPoints.add(0);
+			aiGames.add(0);
 			aiGameTurns.add(0);
+			aiWinGameTurns.add(0);
 			aiMinTurns.add(Integer.MAX_VALUE);
 			aiMaxTurns.add(Integer.MIN_VALUE);
 		}
 		
-		int gameTurns = 0;
+		//int gameTurns = 0;
 		int hibak = 0;
 		
 		Table board = null;
@@ -149,18 +198,28 @@ public class TestMain {
 			board = new Table();
 			MapXMLParser.readCatanMap(new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "catan_base_map.xml"), board);
 			
+			ArrayList<Integer> competitorList = new ArrayList<>();
+			for(int i = 0; i < competitorCnt; i++) {
+				competitorList.add(i);
+			}
+			if(random){
+				Collections.shuffle(competitorList);
+			}
+			while(competitorList.size() > playerCnt){
+				competitorList.remove(competitorList.size() - 1);
+			}
+			
 			playerList.clear();
 			pclist.clear();
 			for(int i = 0; i < playerCnt; i++) {
-				Player p = new Player("AI0" + (i + 1), (i + 1), board);
+				Player p = new Player("AI0" + (competitorList.get(i)), competitorList.get(i), board);
 				playerList.add(p);
 				p.setStupidity(isStupid);
 			}
-			
 			ArrayList<AiController> ais = new ArrayList<>();
 			
 			for(int i = 0; i < playerCnt; i++) {
-				AiController ai = new AiController(board, playerList.get(i), playerList, paramSets.get(i));
+				AiController ai = new AiController(board, playerList.get(i), playerList, paramSets.get(competitorList.get(i)));
 				pclist.add(ai);
 				ais.add(ai);
 				playerList.get(i).setPlayerController(ai);
@@ -170,9 +229,6 @@ public class TestMain {
 			DevCardShop.initializeShop();
 			
 			GameForTest.initialize(playerList, ais, board);
-			
-			if(random)
-				Collections.shuffle(pclist);
 			
 			for(int i = 0; i < pclist.size(); i++){
 				pclist.get(i).firstturn();
@@ -200,18 +256,21 @@ public class TestMain {
 					tmpTurns++;
 				}
 			} catch (GameEndsException e) {
-				gameTurns += tmpTurns;
+				//gameTurns += tmpTurns;
 				for(int i = 0; i < playerCnt; i++){
 					int currentPoint = playerList.get(i).getPoints();
-					aiPoints.set(i, aiPoints.get(i) + currentPoint);
+					int currentId = playerList.get(i).getId();
+					aiGames.set(currentId, aiGames.get(currentId) + 1);
+					aiGameTurns.set(currentId, aiGameTurns.get(currentId) + tmpTurns);
+					aiPoints.set(currentId, aiPoints.get(currentId) + currentPoint);
 					if(currentPoint >= 10){
-						aiWins.set(i, aiWins.get(i) + 1);
-						aiGameTurns.set(i, aiGameTurns.get(i) + tmpTurns);
-						if(aiMaxTurns.get(i) < tmpTurns){
-							aiMaxTurns.set(i, tmpTurns);
+						aiWins.set(currentId, aiWins.get(currentId) + 1);
+						aiWinGameTurns.set(currentId, aiWinGameTurns.get(currentId) + tmpTurns);
+						if(aiMaxTurns.get(currentId) < tmpTurns){
+							aiMaxTurns.set(currentId, tmpTurns);
 						}
-						if(aiMinTurns.get(i) > tmpTurns){
-							aiMinTurns.set(i, tmpTurns);
+						if(aiMinTurns.get(currentId) > tmpTurns){
+							aiMinTurns.set(currentId, tmpTurns);
 						}
 						if(currentPoint > 10)
 							hibak++;
@@ -243,9 +302,13 @@ public class TestMain {
 		}
 		GregorianCalendar endTime = new GregorianCalendar();
 		System.out.println("");
-		for(int i = 0; i < playerCnt; i++){
-			if(aiWins.get(i) > 0)
-				System.out.println(playerList.get(i) + " wins: " + aiWins.get(i) + " (" + (int)((aiWins.get(i) / (double)turns) * 100) + "%)" + "\tmin: " + aiMinTurns.get(i) + "\t\tmax: " + aiMaxTurns.get(i) + "\t\tavg: " + aiGameTurns.get(i) / aiWins.get(i) + "\t\tpoints / turns: " + (double)(aiPoints.get(i)) / (double)(gameTurns) + "\t" + paramSets.get(i).toString());
+		for(int i = 0; i < competitorCnt; i++){
+			if(aiGames.get(i) == 0)
+				System.out.println("AI" + i + " nem kerult sorra");
+			else if(aiWins.get(i) == 0)
+				System.out.println("AI" + i + " games: " + aiGames.get(i) + " wins: 0 (0%)" + "\tmin: -\tmax: -\tavg: -\t\tpoints / turns: " + (double)(aiPoints.get(i)) / (double)(aiGameTurns.get(i)) + "\t" + paramSets.get(i).toString());
+			else 
+				System.out.println("AI" + i + " games: " + aiGames.get(i) + " wins: " + aiWins.get(i) + " (" + (int)((aiWins.get(i) / (double)aiGames.get(i)) * 100) + "%)" + "\tmin: " + aiMinTurns.get(i) + "\tmax: " + aiMaxTurns.get(i) + "\tavg: " + aiWinGameTurns.get(i) / aiWins.get(i) + "\t\tpoints / turns: " + (double)(aiPoints.get(i)) / (double)(aiGameTurns.get(i)) + "\t" + paramSets.get(i).toString());
 		}
 		long difTime = endTime.getTimeInMillis() - startTime.getTimeInMillis();
 		int hour = (int)TimeUnit.MILLISECONDS.toHours(difTime);
